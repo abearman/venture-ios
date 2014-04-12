649bf51b0c5336e296953ed63d308d1d0c58f3f8
Amy Bearman <amy@hyperfine.com>
2014-04-12 10:38:33 -0700

    Got rid of unnecessary Placemark instance variable

--- Venture/Venture/VentureViewController.m
+++ Venture/Venture/VentureViewController.m
@@ -27,12 +27,15 @@
     
     CLLocationManager *locationManager;
     CLLocation *currentLocation;
+    CLGeocoder *geocoder;
+    CLPlacemark *placemark;
 }
 
 - (void) viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     locationManager = [[CLLocationManager alloc] init];
+    geocoder = [[CLGeocoder alloc] init];
 }
 
 - (IBAction)getCurrentLocation:(UIButton *)sender {
@@ -61,59 +64,21 @@
     }*/
 }
 
-
-///////////////////////////////////
-
-- (void) currentLocationIdentifier {
-    //---- For getting current gps location
-    locationManager = [[CLLocationManager alloc] init];
-    locationManager.delegate = self;
-    locationManager.distanceFilter = kCLDistanceFilterNone;
-    locationManager.desiredAccuracy = kCLLocationAccuracyBest;
-    [locationManager startUpdatingLocation];
-}
-
-
-- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
-{
-    currentLocation = [locations objectAtIndex:0];
-    [locationManager stopUpdatingLocation];
-    CLGeocoder *geocoder = [[CLGeocoder alloc] init] ;
-    [geocoder reverseGeocodeLocation:currentLocation completionHandler:^(NSArray *placemarks, NSError *error)
-     {
-         if (!(error))
-         {
-             CLPlacemark *placemark = [placemarks objectAtIndex:0];
-             NSLog(@"\nCurrent Location Detected\n");
-             NSLog(@"placemark %@",placemark);
-             NSString *locatedAt = [[placemark.addressDictionary valueForKey:@"FormattedAddressLines"] componentsJoinedByString:@", "];
-             NSString *Address = [[NSString alloc]initWithString:locatedAt];
-             NSString *Area = [[NSString alloc]initWithString:placemark.locality];
-             NSString *Country = [[NSString alloc]initWithString:placemark.country];
-             NSString *CountryArea = [NSString stringWithFormat:@"%@, %@", Area,Country];
-             NSLog(@"%@",CountryArea);
-         }
-         else
-         {
-             NSLog(@"Geocode failed with error %@", error);
-             NSLog(@"\nCurrent Location Not Detected\n");
-             //return;
-             //CountryArea = NULL;
-         }
-         /*---- For more results
-          placemark.region);
-          placemark.country);
-          placemark.locality);
-          placemark.name);
-          placemark.ocean);
-          placemark.postalCode);
-          placemark.subLocality);
-          placemark.location);
-          ------*/
-     }];
-}
-
 - (IBAction)searchButtonClicked:(id)sender {
+    NSString *specifiedLoc = self.searchBar.text;
+    [geocoder geocodeAddressString:specifiedLoc
+                 completionHandler:^(NSArray *placemarks, NSError *error) {
+                     if (!error) {
+                         for(CLPlacemark *placemark in placemarks){
+                             NSLog(@"%@",[placemark description]);
+                             currentLocation = placemark.location; // Updates current location to their manual address
+                         }
+                     } else {
+                         NSLog(@"There was a forward geocoding error\n%@",
+                               [error localizedDescription]);
+                     }
+                 }
+    ];
     
 }
 
@@ -127,18 +92,6 @@
 
 - (IBAction)noButtonClicked:(id)sender {
     
-}
-
-- (void)startSignificantChangeUpdates
-{
-    // Create the location manager if this object does not
-    // already have one.
-    
-    /*if (nil == locationManager)
-        locationManager = [[CLLocationManager alloc] init];
-    
-    locationManager.delegate = self;
-    [locationManager startMonitoringSignificantLocationChanges];*/
 }
 
 @end
