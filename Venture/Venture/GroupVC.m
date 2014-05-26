0270d12c1e79f617a4610085d92a7c82ad793693
amybear <amy@hyperfine.com>
2014-05-25 17:43:17 -0700

    Have the physics of pan gestures working for groups

--- Venture/Venture/GroupVC.m
+++ Venture/Venture/GroupVC.m
@@ -21,14 +21,48 @@
     UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(swipeLeft:)];
     swipeLeft.direction = UISwipeGestureRecognizerDirectionLeft;
     
+    UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panDetected:)];
+    
+    [pan requireGestureRecognizerToFail:swipeRight];
+    [pan requireGestureRecognizerToFail:swipeLeft];
+    
     [self.centerView addGestureRecognizer:swipeRight];
     [self.centerView addGestureRecognizer:swipeLeft];
+    [self.centerView addGestureRecognizer:pan];
+}
+
+- (void) panDetected:(UIPanGestureRecognizer *)gesture {
+    NSLog(@"Pan!");
+    
+    CGPoint translation = [gesture translationInView:gesture.view];
+    CGFloat xTranslate = gesture.view.center.x + translation.x;
+    gesture.view.center = CGPointMake(xTranslate, gesture.view.center.y);
+    [gesture setTranslation:CGPointMake(0, 0) inView: gesture.view];
+    
+    if (self.centerView.frame.origin.x < 0.0) {
+        self.leftView.hidden = YES;
+        self.rightView.hidden = NO;
+    } else {
+        self.leftView.hidden = NO;
+        self.rightView.hidden = YES;
+    }
+    
+    if (gesture.state == UIGestureRecognizerStateEnded) {
+        NSLog(@"Herp");
+        [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
+                         animations:^{
+                             self.centerView.frame = CGRectMake(0, self.centerView.frame.origin.y, self.centerView.frame.size.width, self.centerView.frame.size.height);
+                         }
+                         completion:nil];
+    }
 }
 
 - (void) swipeRight:(UISwipeGestureRecognizer *)gesture {
+     NSLog(@"Right swipe!");
     CGFloat x = self.centerView.frame.origin.x;
     if (x == 0.0) {
         self.leftView.hidden = NO;
+        self.rightView.hidden = YES;
         [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
                          animations:^{
                              self.centerView.frame = CGRectMake(240.0, self.centerView.frame.origin.y, self.centerView.frame.size.width, self.centerView.frame.size.height);
@@ -44,6 +78,7 @@
 }
 
 - (void) swipeLeft:(UISwipeGestureRecognizer *)gesture {
+     NSLog(@"Left swipe!");
     CGFloat x = self.centerView.frame.origin.x;
     if (x == 240.0) {
         [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
@@ -53,6 +88,7 @@
                          completion:nil];
     } else if (x == 0.0) {
         self.leftView.hidden = YES;
+        self.rightView.hidden = NO;
         [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
                          animations:^{
                              self.centerView.frame = CGRectMake(-240.0, self.centerView.frame.origin.y, self.centerView.frame.size.width, self.centerView.frame.size.height);
@@ -65,36 +101,7 @@
     NSLog(@"X: %f", self.centerView.frame.origin.x);
     
     CGFloat x = self.centerView.frame.origin.x;
-    
-    if (x == 0) {
-        if (xVelocity >= 500.0) {
-            [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
-                             animations:^{
-                                 self.centerView.frame = CGRectMake(240.0, self.centerView.frame.origin.y, self.centerView.frame.size.width, self.centerView.frame.size.height);
-                             }
-                             completion:nil];
-        } else if (xVelocity <= -500.0) {
-            [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
-                             animations:^{
-                                 self.centerView.frame = CGRectMake(-240.0, self.centerView.frame.origin.y, self.centerView.frame.size.width, self.centerView.frame.size.height);
-                             }
-                             completion:nil];
-        }
-    } else if (x == 240.0) {
-        if (xVelocity >= 500.0) {
-            [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
-                             animations:^{
-                                 self.centerView.frame = CGRectMake(240.0, self.centerView.frame.origin.y, self.centerView.frame.size.width, self.centerView.frame.size.height);
-                             }
-                             completion:nil];
-        } else if (xVelocity <= -500.0) {
-            [UIView animateWithDuration:0.4 delay:0 options:UIViewAnimationOptionCurveEaseOut
-                             animations:^{
-                                 self.centerView.frame = CGRectMake(0, self.centerView.frame.origin.y, self.centerView.frame.size.width, self.centerView.frame.size.height);
-                             }
-                             completion:nil];
-        }
-    }
+ 
     
     /*else if (xVelocity <= -500.0 && x <= 160.0 && x >= -90.0) {
         self.leftView.hidden = YES;
